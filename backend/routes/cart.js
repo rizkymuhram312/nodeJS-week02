b8cca08512fcc0521826b700ab514fc7db552230
Ahmad Mardiana <amdtkj@gmail.com>
2021-02-12 10:41:57 +0700

    add apiCartOrder di controller ( cara Pak Idan )

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,11 +1,16 @@
 import { response, Router } from 'express';
 import cartController from '../controllers/cart.controller'
 import orderDetailController from '../controllers/oderDetail.controller'
+import apiCart from "../controllers/apiCartOrder";
+
 
 const router = Router()
 router.get('/',cartController.findAllCart)
 router.get('/all',cartController.findAllCartWOrdi)
 router.post('/',cartController.cartCheck,orderDetailController.addProductToOrderDetail);
+
+router.post('/addCart',apiCart.createCartItems,cartController.findAllCart);
+
 //router.get('/users-address',usersController.findUserWAddress);
 
 export default(router)
